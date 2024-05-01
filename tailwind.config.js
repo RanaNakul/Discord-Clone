fbb2138a822482599916f7e613d9487a92135ec2
Nakul Rana <102985207+RanaNakul@users.noreply.github.com>
2024-05-01 16:36:38 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1600px',
+        '4xl': '2000px',
+      },
+    },
+  },
+  plugins: [],
+}
